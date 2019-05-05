7f2c43c3a91e98d5966d25b87ce7dd1ef484ed00
font <font@g.com>
2019-05-05 14:50:07 +0000

    only users

--- db/migrate/20190323092314_create_users.rb
+++ db/migrate/20190323092314_create_users.rb
@@ -3,7 +3,13 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-
+      t.string :provider
+      t.string :uid
+      t.string :created_at
+      t.string :updated_at
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190323101059) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20190323092314) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
 end
